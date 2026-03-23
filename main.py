904c0e4637b084d502cef5435af8b48dc8cc29e5
triinnq128 <giglenguyenlake121299@gmail.com>
2026-03-23 16:40:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 
+import gspread
+from google.oauth2.service_account import Credentials
+
+# scope
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# load credentials
+creds = Credentials.from_service_account_file(
+    "sheet-bot.json",
+    scopes=scopes
+)
+
+client = gspread.authorize(creds)
+
+# mở file
+sheet = client.open("trading_price_board_vci").sheet1
+
+# ghi data
+sheet.append_row(["Hello", "World"])
